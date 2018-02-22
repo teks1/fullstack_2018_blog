53fdfb80fa616847676b0bf4243f3dc8e3d51f11
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2018-02-22 18:29:35 +0200

    osa4_02

--- index.js
+++ index.js
@@ -4,43 +4,31 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 if ( process.env.NODE_ENV !== 'production' ) {
     require('dotenv').config()
 }
 
-module.exports = Blog
-
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.logger)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.error)
 
 const PORT = 3003
 app.listen(PORT, () => {
